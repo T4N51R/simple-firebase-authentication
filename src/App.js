14f9e756b9642004734f6caacf494f50433e6d47
Tanvir Ahmed <ahmedtanvir290@gmail.com>
2022-04-06 15:04:26 +0600

    Log In with Google done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
-
+import app from './firebase.init';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { useState } from 'react';
+const auth = getAuth(app)
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Practicing Google Firebase Authentication</h1>
+  const [user, setUser] = useState({});
+  const providerGoogle = new GoogleAuthProvider();
 
+  const handleGoogleLogin = () => {
+    signInWithPopup(auth, providerGoogle)
+      .then(result => {
+        const user = result.user
+        setUser(user);
+      }).catch(error => {
+        console.log(error);
+      })
+  }
+  return (
+    <div >
+      <h1>Practicing Google Firebase Authentication</h1>
+      <button onClick={handleGoogleLogin}>Log In with Google</button>
+      <p>Name:{user.displayName} </p>
     </div>
   );
 }
