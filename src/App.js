8cd6d55969181a6f8a803ee54b4875842b8dbd26
Tanvir Ahmed <ahmedtanvir290@gmail.com>
2022-04-06 15:12:09 +0600

    Log In with Github done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import { useState } from 'react';
 const auth = getAuth(app)
 
 function App() {
   const [user, setUser] = useState({});
   const providerGoogle = new GoogleAuthProvider();
+  const providerGithub = new GithubAuthProvider()
 
+  // Google Log in button 
   const handleGoogleLogin = () => {
     signInWithPopup(auth, providerGoogle)
       .then(result => {
@@ -17,11 +19,25 @@ function App() {
         console.log(error);
       })
   }
+  // Handle Github login 
+  const handleGithubLogin = () => {
+    signInWithPopup(auth, providerGithub)
+      .then(result => {
+        const user = result.user;
+        setUser(user);
+        console.log(user)
+      }).catch(error => {
+        console.log(error)
+      })
+  }
+
   return (
     <div >
       <h1>Practicing Google Firebase Authentication</h1>
       <button onClick={handleGoogleLogin}>Log In with Google</button>
-      <p>Name:{user.displayName} </p>
+      <button onClick={handleGithubLogin}>Log In with Github</button>
+      <h1>Welcome {user.displayName}</h1>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
