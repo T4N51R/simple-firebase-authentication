61bdb57206ae087a68190616c997778871c44b5e
Tanvir Ahmed <ahmedtanvir290@gmail.com>
2022-04-06 15:42:33 +0600

    Log In with Facebook done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { FacebookAuthProvider, getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import { useState } from 'react';
 const auth = getAuth(app)
 
@@ -8,7 +8,7 @@ function App() {
   const [user, setUser] = useState({});
   const providerGoogle = new GoogleAuthProvider();
   const providerGithub = new GithubAuthProvider()
-
+  const providerFacebook = new FacebookAuthProvider()
   // Google Log in button 
   const handleGoogleLogin = () => {
     signInWithPopup(auth, providerGoogle)
@@ -31,11 +31,27 @@ function App() {
       })
   }
 
+
+  // Login with FaceBook 
+  const handleFacebookLogin = () => {
+    signInWithPopup(auth, providerFacebook)
+      .then(result => {
+        const user = result.user;
+        setUser(user);
+        console.log(user)
+      }).catch(error => {
+        console.log(error)
+      })
+  }
+
+
+
   return (
     <div >
       <h1>Practicing Google Firebase Authentication</h1>
       <button onClick={handleGoogleLogin}>Log In with Google</button>
       <button onClick={handleGithubLogin}>Log In with Github</button>
+      <button onClick={handleFacebookLogin}>Log In with Facebook</button>
       <h1>Welcome {user.displayName}</h1>
       <img src={user.photoURL} alt="" />
     </div>
